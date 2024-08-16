692104f4a80e45792b0f733abe9ed20dd06d9f19
Perrin Letembet <perrinletembet.luquet@gmail.com>
2024-08-16 06:29:14 -0700

    Bubble sort

--- sort.h
+++ sort.h
@@ -8,7 +8,11 @@
 #define UP 0
 #define DOWN 1
 
-/* Enumeration of boolean values */
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
 typedef enum bool
 {
 	false = 0,
@@ -29,17 +33,22 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* Helper for swapping */
-void swap_ints(int *a, int *b);
-
 /* Printing helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/* Regular functions prototypes */
+/* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif /* SORT_H */
